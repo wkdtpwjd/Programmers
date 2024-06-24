fc634f4e8c1ed3f759482da806273cff3819b06c
wkdtpwjd <145098385+wkdtpwjd@users.noreply.github.com>
2024-06-24 14:47:31 +0900

    [level 2] Title: 귤 고르기, Time: 36.12 ms, Memory: 21.3 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -1,14 +1,18 @@
-def solution(k, tangerine):
-    from collections import Counter
-    
-    tangerine_counts = Counter(tangerine)
-    sorted_counts = sorted(tangerine_counts.values(), reverse=True)
-    
-    cnt = 0
-    for count in sorted_counts:
-        k -= count
-        cnt += 1
-        if k <= 0:
-            break
-    return cnt
+def solution(k,tangerine):
+    graph = {}
+    for i in tangerine:
+        if i not in graph:
+            graph[i] = 1
+        else:
+            graph[i] += 1
+    graph = sorted(graph.items(),key=lambda x:x[1],reverse=True)
+    # 각 크기별 몇개인지 세서 개수가 가장많은것 순으로 정렬
+    type = 0 # 종류의 수
+    cnt = 0 # 현재까지 귤의 수
+
+    for i in graph:
+        cnt += i[1]
+        type += 1
+        if cnt >= k:
+            return type
 
