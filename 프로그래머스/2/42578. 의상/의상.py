a334938e445caa752459d6c6b72e3f6a9ff3d867
wkdtpwjd <145098385+wkdtpwjd@users.noreply.github.com>
2024-07-06 19:09:28 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,14 +1,11 @@
 def solution(clothes):
-    hash = {} # 의상의 종류가 키인 해시
-    for a,b in clothes:
-        if b not in hash:
-            hash[b] = 1
+    graph = {}
+    for i in clothes:
+        if i[1] not in graph:
+            graph[i[1]] = 1
         else:
-            hash[b] += 1
-    lst = []
-    for i in hash:
-        lst.append(hash[i] + 1) # 각 의상의 종류개수 + 선택안할수도 있는 경우하나 더하기
+            graph[i[1]] += 1
     answer = 1
-    for i in lst:
-        answer *= i  # 각 경우의 수 다곱하기
-    return answer-1 # 최소하나의 의상은 입어야 하므로 아무것도 없는 경우 하나빼기
+    for i in graph.items() :
+        answer *= i[1]+1
+    return answer-1
