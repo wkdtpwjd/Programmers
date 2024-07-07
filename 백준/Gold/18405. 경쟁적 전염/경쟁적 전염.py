e8406072100dfaf6eea18fc96d73f7d8f9192b85
wkdtpwjd <145098385+wkdtpwjd@users.noreply.github.com>
2024-07-07 10:56:30 +0900

    [Gold V] Title: 경쟁적 전염, Time: 180 ms, Memory: 112956 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
@@ -1,32 +1,28 @@
-from collections import deque
-
+n,k = map(int,input().split())
+arr = [list(map(int,input().split())) for _ in range(n)]
+s,x,y = map(int,input().split())
 
 di = [-1,0,1,0]
 dj = [0,1,0,-1]
-n,k = map(int,input().split()) #  =n*n 배열이고  1부터 k까지 바이러스 있음
-arr = [list(map(int,input().split())) for _ in range(n)]
-visited = [[0] * n for _ in range(n)]
-s,find_x,find_y= map(int,input().split())
 
 
-# 일단 배열에서 0이 아닌 지점을 찾아서 담을 배열
-lst = []
+
+q = []
 for i in range(n):
     for j in range(n):
         if arr[i][j] != 0 :
-            lst.append((arr[i][j],i,j,0))
-lst.sort(key=lambda x:x[0])  # 바이러스의 숫자를 기준으로 오름차순(낮은숫자부터 전염되므로)
-# [(1, 0, 0), (2, 0, 2), (3, 2, 0)]
-q = deque(lst)
+            q.append((arr[i][j],i,j,0))
+q.sort()
 
 while q:
-    num,x,y,second = q.popleft()
+    virus_num,r,l,second = q.pop(0)
     if second == s:
         break
     for d in range(4):
-        nx = x + di[d]
-        ny = y + dj[d]
-        if 0<=nx<n and 0<=ny<n and arr[nx][ny] == 0:
-            arr[nx][ny] = num
-            q.append((num,nx,ny,second+1))
-print(arr[find_x-1][find_y-1])
+        nr = r + di[d]
+        nl = l + dj[d]
+        if 0<=nr<n and 0<=nl<n and arr[nr][nl] == 0:
+            arr[nr][nl] = virus_num
+            q.append((virus_num,nr,nl,second+1))
+
+print(arr[x-1][y-1])
