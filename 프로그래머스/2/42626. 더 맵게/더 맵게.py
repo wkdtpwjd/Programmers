5389b2e8defe7d7f46bd08ac137371ced72cc12d
wkdtpwjd <145098385+wkdtpwjd@users.noreply.github.com>
2024-07-07 11:08:57 +0900

    [level 2] Title: 더 맵게, Time: 1472.04 ms, Memory: 51.8 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,18 +1,18 @@
 import heapq
 
 def solution(scoville,K):
-    answer = 0
-    heapq.heapify(scoville)  # 스코빌배열을 heap으로 만들기
+    cnt = 0
+    scoville.sort()
+    heapq.heapify(scoville)
     while len(scoville) >= 2:
-        min_v = heapq.heappop(scoville)
-        if min_v >= K:  # 가장작은 요소가 K 이상이면 섞은횟수 출력
-            return answer
-        else:
-            min_v2 = heapq.heappop(scoville)
-            heapq.heappush(scoville,min_v+(min_v2*2))
-            answer += 1 # 횟수세어주기
-    # 하나가남았을때 K이상인지 확인하기
+        min_v1 = heapq.heappop(scoville)
+        if min_v1 >= K :
+            return cnt
+        min_v2 = heapq.heappop(scoville)
+        heapq.heappush(scoville,min_v1+(min_v2*2))
+        cnt += 1
+
     if scoville[0] >= K:
-        return answer
+        return cnt
     else:
         return -1
