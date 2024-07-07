17e60194ff4170f6b05da4f78c2b821ffc9499bc
wkdtpwjd <145098385+wkdtpwjd@users.noreply.github.com>
2024-07-07 13:06:12 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -1,9 +1,9 @@
 def solution(bridge_length,weight,truck_weights):
     q = [0] * bridge_length
-    second = 0
     current_w = 0
+    second = 0
     while truck_weights:
-        second += 1  # 1초가 지나면 큐(다리)의 맨앞의 트럭도 빠짐
+        second += 1
         current_w -= q.pop(0)
         if current_w + truck_weights[0] <= weight:
             q.append(truck_weights[0])
