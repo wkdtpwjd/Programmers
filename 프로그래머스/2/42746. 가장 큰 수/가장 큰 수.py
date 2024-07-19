14736f55c08bf0bf62802db13d19c6e1fcea249c
wkdtpwjd <145098385+wkdtpwjd@users.noreply.github.com>
2024-07-19 15:41:05 +0900

    [level 2] Title: 가장 큰 수, Time: 58.03 ms, Memory: 27.8 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,9 +1,9 @@
 def solution(numbers):
-    numbers = list(map(str,numbers))
+    numbers = [str(i) for i in numbers]
+
     numbers = sorted(numbers,key=lambda x:x*4,reverse=True)
-    answer = ''
-    for i in numbers:
-        answer += i
-    if answer[0] == '0':
+    string = ''.join(numbers)
+    if string[0] == '0':
         return '0'
-    return answer
+    else:
+        return string
